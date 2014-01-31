cc480e38eb411ac544a83f6438fa7d3ac7676f94
Daniel Durante <me@danieldurante.com>
2014-01-31 14:20:31 -0500

    fix(grunt): Adds readme command to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,5 +17,15 @@ module.exports = function( grunt ) {
 
         // Register db command (runs one after the other)
         grunt.registerTask( 'db', [ 'db:rebase', 'db:seed' ] );
+
+        grunt.registerTask( 'readme', 'Displays helpful information', function ( ) {
+            console.log( 'Installation instructions:' );
+            console.log( '1. From your project folder run `clever install clever-orm`' );
+            console.log( '' );
+            console.log( '2. In the config file for your desired environment (ie. backend/config/local.json), update the clever-orm object with the details for your database.' );
+            console.log( '' );
+            console.log( '3. From your project\'s `backend` folder, run `NODE_ENV=local grunt db`.' );
+            console.log( 'The database tables for your modules should now be installed and seeded with data!' );
+        } );
     }];
 };
