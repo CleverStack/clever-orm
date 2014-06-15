ab7ea3e43bc6527e357505ce18db0d634219d49e
Richard Gustin <richard.gustin86@gmail.com>
2014-06-15 21:41:07 +1000

    refactor(cleanup): Code climate

--- bin/rebase.js
+++ bin/rebase.js
@@ -1,36 +1,51 @@
 var injector    = require( 'injector' )
-  , fs          = require( 'fs' )
   , utils       = require( 'utils' )
+  , async       = require( 'async' )
+  , config      = require( 'config' )
+  , path        = require( 'path' )
+  , ormUtils    = require( path.resolve( path.join( __dirname, '..', 'lib', 'utils.js' ) ) )
   , env         = utils.bootstrapEnv()
-  , moduleLdr   = env.moduleLoader
-  , moduleName  = process.argv && process.argv[ 2 ] != 'null' ? process.argv[ 2 ] : false;
+  , moduleLdr   = env.moduleLoader;
 
 // Rebase once our modules have loaded
 moduleLdr.on( 'modulesLoaded', function() {
+    var sequelize = injector.getInstance( 'sequelize' );
+
     console.log('Forcing Database to be created! (Note: All your data will disapear!)');
+    async.waterfall(
+        [
+            function createDatabase( callback ) {
+                var query = 'CREATE DATABASE ' + ( config[ 'clever-orm' ].db.options.dialect === 'mysql' ? 'IF NOT EXISTS ' : '' ) + config[ 'clever-orm' ].db.database;
 
-    // Force a sync
-    injector.getInstance( 'sequelize' )
-        .sync( { force: true } )
-        .success(function () {
-        	console.log( 'Database is rebased' );
-            // @TODO implement dialect specific SQL running after rebase (triggers etc)
+                sequelize.query( query, { raw: true } )
+                    .success( function() {
+                        callback( null );
+                    })
+                    .error( callback );
+            },
 
-            env.moduleLoader.shutdown();
-        })
-        .error(function( err ) {
-            console.error('Error ' + env.config['clever-orm'].db.options.dialect, err);
-            
-            env.moduleLoader.shutdown();
-        });
+            function rebaseDatabase( callback ) {
+                sequelize
+                    .sync( { force: true } )
+                    .success( function() {
+                        callback( null );
+                    })
+                    .error( callback );
+            }
+        ],
+        function shutdown( err ) {
+            if ( err === null ) {
+                console.log( 'Database is rebased' );
+                env.moduleLoader.shutdown();
+            } else {
+                console.error('Error ' + env.config['clever-orm'].db.options.dialect, err);
+                env.moduleLoader.shutdown();
+            }
+        }
+    );
 });
 
-// Allow loading only one model at a time to rebase
-if ( moduleName ) {
-    env.packageJson.bundledDependencies.length = 0
-    env.packageJson.bundledDependencies.push( 'clever-orm', env );
-    env.packageJson.bundledDependencies.push( moduleName, env );
-}
+ormUtils.supportSingleModule( env, process.argv && process.argv[ 2 ] != 'null' ? process.argv[ 2 ] : false );
 
 // Load
 moduleLdr.loadModules();
--- bin/seedModels.js
+++ bin/seedModels.js
@@ -1,13 +1,10 @@
-var injector    = require( 'injector' )
-  , fs          = require( 'fs' )
-  , path        = require( 'path' )
-  , crypto      = require( 'crypto' )
+var path        = require( 'path' )
+  , ormUtils    = require( path.resolve( path.join( __dirname, '..', 'lib', 'utils.js' ) ) )
   , async       = require( 'async' )
   , utils       = require( 'utils' )
   , env         = utils.bootstrapEnv()
   , moduleLdr   = env.moduleLoader
-  , inflect     = require( 'i' )()
-  , moduleName  = process.argv && process.argv[ 2 ] != 'null' ? process.argv[ 2 ] : false;
+  , inflect     = require( 'i' )();
 
 // Seed once our modules have loaded
 moduleLdr.on( 'modulesLoaded', function() {
@@ -15,15 +12,15 @@ moduleLdr.on( 'modulesLoaded', function() {
       , models = require( 'models' );
 
     var assocMap = {};
-    Object.keys(seedData).forEach(function( modelName ) {
-        assocMap[modelName] = [];
+    Object.keys( seedData ).forEach(function( modelName ) {
+        assocMap[ modelName ] = [];
     });
 
     async.forEachSeries(
-        Object.keys(seedData),
+        Object.keys( seedData ),
         function forEachModelType( modelName, cb ) {
             var ModelType = models[ modelName.replace( 'Model', '' ) ]
-              , Models = seedData[modelName];
+              , Models = seedData[ modelName ];
 
             if ( !ModelType || !Models ) {
                 return cb();
@@ -35,81 +32,81 @@ moduleLdr.on( 'modulesLoaded', function() {
                     var assocs = data.associations;
                     delete data.associations;
 
-                    ModelType.create(data).then(function( model ) {
-                        data.associations = assocs;
+                    ModelType.create( data )
+                        .then(function( model ) {
+                            data.associations = assocs;
 
-                        console.log('Created ' + modelName);
-                        assocMap[modelName].push(model);
-                        if ( data.associations !== undefined ) {
-                            var assocLength = Object.keys(data.associations).length,
-                                called = 0;
+                            console.log( 'Created ' + modelName );
+                            assocMap[ modelName ].push( model );
+                            if ( data.associations !== undefined ) {
+                                var assocLength = Object.keys( data.associations ).length
+                                  , called      = 0;
 
-                            Object.keys(data.associations).forEach(function( assocModelName ) {
-                                var required = data.associations[assocModelName]
-                                    , associations = [];
+                                Object.keys( data.associations ).forEach( function( assocModelName ) {
+                                    var required     = data.associations[ assocModelName ]
+                                      , associations = [];
 
-                                assocMap[assocModelName].forEach(function( m ) {
-                                    var isMatched = null;
+                                    assocMap[ assocModelName ].forEach( function( m ) {
+                                        var isMatched = null;
 
-                                    Object.keys(required).forEach(function( reqKey ) {
-                                        if ( isMatched !== false ) {
-                                            if ( m[reqKey] === required[reqKey] ) {
-                                                isMatched = true;
-                                            } else {
-                                                isMatched = false;
+                                        Object.keys( required ).forEach( function( reqKey ) {
+                                            if ( isMatched !== false ) {
+                                                if ( m[ reqKey ] === required[ reqKey ] ) {
+                                                    isMatched = true;
+                                                } else {
+                                                    isMatched = false;
+                                                }
                                             }
+                                        });
+
+                                        if ( isMatched ) {
+                                            associations.push( m );
                                         }
                                     });
 
-                                    if ( isMatched ) {
-                                        associations.push(m);
+                                    if ( associations.length ) {
+                                        var funcName = 'set' + inflect.pluralize( assocModelName );
+
+                                        // Handle hasOne
+                                        if ( typeof model[ funcName ] !== 'function' ) {
+                                            funcName = 'set' + assocModelName;
+                                            associations = associations[ 0 ];
+                                        }
+                                        
+                                        // strip "Model" from funcName
+                                        funcName = funcName.replace( /(Model)$/g,'' );
+
+                                        console.log( 'Calling ' + funcName );
+                                        model[ funcName ]( associations )
+                                            .success(function() {
+                                                called++;
+
+                                                if ( called == assocLength ) {
+                                                    modelCb( null );
+                                                }
+                                            })
+                                            .error( modelCb );
                                     }
                                 });
-
-                                if ( associations.length ) {
-                                    var funcName = 'set' + inflect.pluralize(assocModelName);
-
-                                    // Handle hasOne
-                                    if ( typeof model[funcName] !== 'function' ) {
-                                        funcName = 'set' + assocModelName;
-                                        associations = associations[0];
-                                    }
-                                    
-                                    // strip "Model" from funcName
-                                    funcName = funcName.replace(/(Model)$/g,'');
-
-                                    console.log('Calling ' + funcName);
-                                    model[funcName](associations).success(function() {
-                                        called++;
-
-                                        if ( called == assocLength )
-                                            modelCb(null);
-                                    }).error(modelCb);
-                                }
-                            });
-                        } else {
-                            modelCb(null);
-                        }
-                    }).catch(modelCb);
+                            } else {
+                                modelCb( null );
+                            }
+                        })
+                        .catch( modelCb );
                 },
                 function forEachModelComplete( err ) {
-                    cb(err);
+                    cb( err );
                 }
             );
         },
         function forEachModelTypeComplete( err ) {
-            console.log(err ? 'Error: ' : 'Seed completed with no errors', err);
+            console.log( err ? 'Error: ' : 'Seed completed with no errors', err );
             env.moduleLoader.shutdown();
         }
     );
 });
 
-// Allow loading only one model at a time to rebase
-if ( moduleName ) {
-    env.packageJson.bundledDependencies.length = 0
-    env.packageJson.bundledDependencies.push( 'clever-orm', env );
-    env.packageJson.bundledDependencies.push( moduleName, env );
-}
+ormUtils.supportSingleModule( env, process.argv && process.argv[ 2 ] != 'null' ? process.argv[ 2 ] : false );
 
 // Load
 moduleLdr.loadModules();
