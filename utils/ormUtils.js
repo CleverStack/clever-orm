ae9cccbc894fdc98d05bcec24acf1daa29399b3a
Richard Gustin <richard@rgnconsulting.com.au>
2015-03-12 07:00:20 +1000

    refactor(model): For node-seed@1.2.1

--- utils/ormUtils.js
+++ utils/ormUtils.js
@@ -1,17 +1,18 @@
 var path     = require('path')
-  , ormLib   = require(path.resolve(path.join(__dirname, '..', 'lib')));
+  , ormLib   = require(path.resolve(path.join(__dirname, '..', 'lib')))
+  , model    = ormLib.model
 
 module.exports  = {
-  find            : ormLib.model.find,
-  save            : ormLib.model.instance.save,
-  create          : ormLib.model.create,
-  update          : ormLib.model.update,
-  destroy         : ormLib.model.destroy,
-  destroyInstance : ormLib.model.instance.destroy,
-  findAll         : ormLib.model.findAll,
-  wrapModel       : ormLib.model.instance.hydrator,
-  setup           : ormLib.model.instance.setup,
-  eagerLoad       : ormLib.model.associations.loaders.eager.load,
-  afterEagerLoad  : ormLib.model.associations.loaders.eager.afterLoad,
-  softDeleteable  : ormLib.model.behaviours.softDeleteable.whereCriteria
+  find            : model.find,
+  save            : model.instance.save,
+  create          : model.create,
+  update          : model.update,
+  destroy         : model.destroy,
+  destroyInstance : model.instance.destroy,
+  findAll         : model.findAll,
+  wrapModel       : model.instance.hydrator,
+  setup           : model.instance.setup,
+  eagerLoad       : model.associations.loaders.eager.load,
+  afterEagerLoad  : model.associations.loaders.eager.afterLoad,
+  softDeleteable  : model.behaviours.softDeleteable.whereCriteria
 };
