66e6e4ac8d830a4466cf0248a2771c215e4c5f3e
Richard Gustin <richard@rgnconsulting.com.au>
2015-10-06 02:57:30 +1000

    fix(postInstall): Script was failing because of multiple dbPackages when postgre was chosen

--- bin/postInstall.js
+++ bin/postInstall.js
@@ -5,6 +5,7 @@ var path    = require('path')
   , cp      = require('child_process')
   , config  = require('config')
   , os      = require('os')
+  , async   = require('async')
   , isWin   = /^win32/.test(os.platform())
   , dialect = config['clever-orm'].db.options.dialect
   , appRoot = path.resolve(path.join(__dirname, '..', '..', '..'))
@@ -16,41 +17,49 @@ var path    = require('path')
 switch(dialect) {
 
 case 'mysql':
-  dbPkg = 'mysql';
+  dbPkg = ['mysql'];
   break;
 case 'mariadb':
-  dbPkg = 'mariasql';
+  dbPkg = ['mariasql'];
   break;
 case 'mssql':
-  dbPkg = 'tedious';
+  dbPkg = ['tedious'];
   break;
 case 'postgres':
-  dbPkg = 'pg pg-hstore';
+  dbPkg = ['pg','pg-hstore'];
   break;
 case 'sqlite':
-  dbPkg = 'sqlite3';
+  dbPkg = ['sqlite3'];
   break;
 }
 
-if (!fs.existsSync(path.join(appRoot, 'node_modules', dbPkg))) {
-  var opts = { cwd: appRoot };
-  if (verbose) {
-    opts.stdio = 'inherit';
-  }
+async.each(
+  dbPkg,
+  function installDbPackage(dbPkg, callback) {
+    if (!fs.existsSync(path.join(appRoot, 'node_modules', dbPkg))) {
+      var opts = { cwd: appRoot };
+      if (verbose) {
+        opts.stdio = 'inherit';
+      }
 
-  var proc = cp.spawn(!isWin ? 'npm' :'npm.CMD', ['i', dbPkg], opts);
-
-  proc.on('close', function(code) {
-    if (code === 0) {
-      pkgJson.dependencies[dbPkg] = '~' + require(path.join(appRoot, 'node_modules', dbPkg, 'package.json')).version;
-      fs.writeFile(pkgPath, JSON.stringify( pkgJson, null, '  '), function(err) {
-        if (err) {
-          console.error(err);
+      cp
+      .spawn(!isWin ? 'npm' :'npm.CMD', ['i', dbPkg], opts)
+      .on('close', function(code) {
+        if (code === 0) {
+          pkgJson.dependencies[dbPkg] = '~' + require(path.join(appRoot, 'node_modules', dbPkg, 'package.json')).version;
+          fs.writeFile(pkgPath, JSON.stringify( pkgJson, null, '  '), function(err) {
+            callback(err, code);
+          });
+        } else {
+          process.exit(code);
         }
-        process.exit(code);
       });
-    } else {
-      process.exit(code);
+    }    
+  },
+  function installationCompleted(err) {
+    if (err) {
+      console.error(err);
     }
-  });
-}
+    process.exit(0);
+  }
+);
